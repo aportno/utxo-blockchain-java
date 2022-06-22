17f33a64caeaba5a80fd73741ec07655fabe5b9f
aportno <alexander.portno@gmail.com>
2022-06-22 18:54:50 -0400

    update

--- src/chapter3/TestCipher_1.java
+++ src/chapter3/TestCipher_1.java
@@ -5,9 +5,13 @@ import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 
+/*
+THIS SCRIPT USES AES FOR EDUCATIONAL PURPOSES ONLY
+ */
+
 public class TestCipher_1 {
     public static void main(String[] args) throws Exception {
-        Cipher cipher = Cipher.getInstance("AES");
+        Cipher first_cipher = Cipher.getInstance("AES");
         KeyGenerator keyGen = KeyGenerator.getInstance("AES");
         SecureRandom secRand = SecureRandom.getInstance("SHA1PRNG");
 
@@ -15,15 +19,15 @@ public class TestCipher_1 {
 
         SecretKey key = keyGen.generateKey();
 
-        cipher.init(Cipher.ENCRYPT_MODE, key);
+        first_cipher.init(Cipher.ENCRYPT_MODE, key);
 
         String message = "If you were a drop of tear in my eyes";
-        byte[] cipherText = cipher.doFinal(message.getBytes());
-        Cipher cipher2 = Cipher.getInstance("AES");
+        byte[] cipherText = first_cipher.doFinal(message.getBytes());
+        Cipher second_cipher = Cipher.getInstance("AES");
 
-        cipher2.init(Cipher.DECRYPT_MODE, key);
+        second_cipher.init(Cipher.DECRYPT_MODE, key);
 
-        byte[] decoded = cipher2.doFinal(cipherText);
+        byte[] decoded = second_cipher.doFinal(cipherText);
         String decodedMessage = new String(decoded);
 
         System.out.println(decodedMessage);
