9462b7112c0d1d2a04bb496a116d6b4ba17e947b
aportno <alexander.portno@gmail.com>
2022-07-10 19:37:09 -0400

    update

--- src/chapter9/MessageAskForBlockchainBroadcast.java
+++ src/chapter9/MessageAskForBlockchainBroadcast.java
@@ -7,9 +7,24 @@ import java.security.PublicKey;
 public class MessageAskForBlockchainBroadcast extends MessageTextBroadcast {
     @Serial
     private final static long serialVersionUID = 1L;
+    private final String uniqueHashID;
+    private final long timeStamp;
 
-    public MessageAskForBlockchainBroadcast(String info, PrivateKey privateKey, PublicKey senderKey, String senderName) {
-        super(info, privateKey, senderKey, senderName);
+    public MessageAskForBlockchainBroadcast(String info, PrivateKey privateKey, PublicKey sender, String walletName) {
+        super(info, privateKey, sender, walletName);
+        timeStamp = UtilityMethods.getTimeStamp();
+        String msg = UtilityMethods.getKeyString(sender) + walletName + timeStamp + UtilityMethods.getUniqueNumber();
+        uniqueHashID = UtilityMethods.messageDigestSHA256_toString(msg);
+    }
+
+    @Override
+    public String getMessageHashID() {
+        return uniqueHashID;
+    }
+
+    @Override
+    public long getTimeStamp() {
+        return timeStamp;
     }
 
     public int getMessageType() {
