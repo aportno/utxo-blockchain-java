d9a693e15b2661c8ac6cab92d256be45d5e3e779
aportno <alexander.portno@gmail.com>
2022-07-10 16:39:17 -0400

    update

--- src/chapter9/Configuration.java
+++ src/chapter9/Configuration.java
@@ -5,8 +5,15 @@ public final class Configuration {
     private static final String HASH_ALGORITHM = "SHA-256";
     private static final String SIGNATURE_ALGORITHM = "SHA256withRSA";
     private static final String KEYPAIR_ALGORITHM = "RSA";
+    private static final double MINING_REWARD = 100.0;
     private static final int PORT = 1117;
+    private static final int SELF_BLOCKS_TO_MINE_LIMIT = 2;
+    private static final int SIGN_IN_BONUS_USERS_LIMIT = 4;
+    private static final int BLOCK_TRANSACTION_UPPER_LIMIT = 100;
+    private static final int BLOCK_TRANSACTION_LOWER_LIMIT = 2;
     private static final int BLOCK_MINING_DIFFICULTY_LEVEL = 20;
+    private static final int THREAD_SLEEP_TIME_SHORT = 100;
+    private static final long MESSAGE_BURIED_TIME_LIMIT = 864_000_000;
 
     public static String getKeyLocation() {
         return KEY_LOCATION;
@@ -31,4 +38,33 @@ public final class Configuration {
     public static int getBlockMiningDifficultyLevel() {
         return BLOCK_MINING_DIFFICULTY_LEVEL;
     }
+
+    public static int getSignInBonusUsersLimit() {
+        return SIGN_IN_BONUS_USERS_LIMIT;
+    }
+
+    public static int getSelfBlocksToMineLimit() {
+        return SELF_BLOCKS_TO_MINE_LIMIT;
+    }
+
+    public static int getBlockTransactionUpperLimit() {
+        return BLOCK_TRANSACTION_UPPER_LIMIT;
+    }
+
+    public static int getBlockTransactionLowerLimit() {
+        return BLOCK_TRANSACTION_LOWER_LIMIT;
+    }
+
+    public static double getMiningReward() {
+        return MINING_REWARD;
+    }
+
+    public static long getMessageBuriedTimeLimit() {
+        return MESSAGE_BURIED_TIME_LIMIT;
+    }
+
+    public static int getThreadSleepTimeShort() {
+        return THREAD_SLEEP_TIME_SHORT;
+    }
+
 }
