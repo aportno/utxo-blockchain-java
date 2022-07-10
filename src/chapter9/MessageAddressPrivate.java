bf26c12c3725b49e772fe622eb08a6d0c6b6b24e
aportno <alexander.portno@gmail.com>
2022-07-10 19:32:36 -0400

    update

--- src/chapter9/MessageAddressPrivate.java
+++ src/chapter9/MessageAddressPrivate.java
@@ -1,15 +1,25 @@
 package chapter9;
 
 import java.io.Serial;
+import java.security.PublicKey;
 import java.util.ArrayList;
 
 public class MessageAddressPrivate extends Message {
     @Serial
     private static final long serialVersionUID = 1L;
     private final ArrayList<KeyNamePair> addresses;
+    private final PublicKey sender;
+    private final PublicKey receiver;
+    private final String uniqueHashID;
+    private final long timeStamp;
 
-    public MessageAddressPrivate(ArrayList<KeyNamePair> addresses) {
+    public MessageAddressPrivate(ArrayList<KeyNamePair> addresses, PublicKey sender, PublicKey receiver) {
         this.addresses = addresses;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.timeStamp = UtilityMethods.getTimeStamp();
+        String msg = UtilityMethods.getKeyString(sender) + UtilityMethods.getKeyString(receiver) + timeStamp + UtilityMethods.getUniqueNumber();
+        uniqueHashID = UtilityMethods.messageDigestSHA256_toString(msg);
     }
 
     public int getMessageType() {
@@ -22,5 +32,24 @@ public class MessageAddressPrivate extends Message {
 
     public boolean isForBroadcast() {
         return false;
+    }
+
+    @Override
+    public String getMessageHashID() {
+        return uniqueHashID;
+    }
+
+    @Override
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    @Override
+    public PublicKey getSenderKey() {
+        return sender;
+    }
+
+    public PublicKey getReceiverKey() {
+        return receiver;
     }
 }
