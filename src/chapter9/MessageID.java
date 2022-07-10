c720d2f21326f3c8b87cbc2ffe95ceefa555eca0
aportno <alexander.portno@gmail.com>
2022-07-10 17:50:25 -0400

    update

--- src/chapter9/MessageID.java
+++ src/chapter9/MessageID.java
@@ -4,7 +4,7 @@ import java.io.Serial;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 
-public class MessageID extends MessageSigned {
+public final class MessageID extends MessageSigned {
     /*
     Used to send the name and public key of a wallet to the message service provider
      */
@@ -14,12 +14,17 @@ public class MessageID extends MessageSigned {
     private final PublicKey sender;
     private final String walletName;
     private final String info;
+    private final String uniqueHashID;
+    private long timeStamp = 0;
 
     public MessageID(PrivateKey privateKey, PublicKey sender, String walletName) {
         this.info = Message.JCOIN_MESSAGE;
         this.sender = sender;
         this.walletName = walletName;
         signature = UtilityMethods.generateSignature(privateKey, this.info);
+        this.timeStamp = UtilityMethods.getTimeStamp();
+        String msg = UtilityMethods.getKeyString(sender) + walletName + UtilityMethods.getUniqueNumber() + timeStamp;
+        this.uniqueHashID = UtilityMethods.messageDigestSHA256_toString(msg);
     }
 
     public String getMessageBody() {
@@ -40,6 +45,18 @@ public class MessageID extends MessageSigned {
 
     public KeyNamePair getKeyNamePair() {
         return new KeyNamePair(this.getPublicKey(), this.getName());
+    }
+
+    public String getMessageHashID() {
+        return uniqueHashID;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public PublicKey getSenderKey() {
+        return sender;
     }
 
     public boolean isValid() {
