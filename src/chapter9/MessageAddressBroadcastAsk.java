8df1f36a8adcda2982bb9289e490d61fa440af77
aportno <alexander.portno@gmail.com>
2022-07-10 19:27:42 -0400

    update

---
+++ src/chapter9/MessageAddressBroadcastAsk.java
@@ -0,0 +1,59 @@
+package chapter9;
+
+import java.io.Serial;
+import java.security.PublicKey;
+
+public class MessageAddressBroadcastAsk extends Message {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private final PublicKey sender;
+    private final String walletName;
+    private final String uniqueHashID;
+    private final long timeStamp;
+
+    public MessageAddressBroadcastAsk(PublicKey sender, String walletName) {
+        this.sender = sender;
+        this.walletName = walletName;
+        timeStamp = UtilityMethods.getTimeStamp();
+        String msg = UtilityMethods.getKeyString(sender) + timeStamp + UtilityMethods.getUniqueNumber();
+        uniqueHashID = UtilityMethods.messageDigestSHA256_toString(msg + walletName);
+    }
+
+    @Override
+    public Object getMessageBody() {
+        return "Request peer information";
+    }
+
+    @Override
+    public int getMessageType() {
+        return Message.ADDRESS_ASK_BROADCAST;
+    }
+
+    @Override
+    public String getMessageHashID() {
+        return uniqueHashID;
+    }
+
+    @Override
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    @Override
+    public PublicKey getSenderKey() {
+        return sender;
+    }
+
+    @Override
+    public boolean isForBroadcast() {
+        return true;
+    }
+
+    public String getWalletName() {
+        return walletName;
+    }
+
+    public KeyNamePair getKeyNamePair() {
+        return new KeyNamePair(sender, walletName);
+    }
+}
