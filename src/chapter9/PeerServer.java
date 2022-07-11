67b37df698a182be98fb3103c359fca2e7902f9b
aportno <alexander.portno@gmail.com>
2022-07-10 22:28:13 -0400

    update

--- src/chapter9/PeerServer.java
+++ src/chapter9/PeerServer.java
@@ -1,4 +1,51 @@
 package chapter9;
 
-public class PeerServer {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.InetAddress;
+
+public class PeerServer implements Runnable {
+    private Wallet wallet;
+    private ServerSocket server;
+    private static String IPAddress;
+    private PeerConnectionManager connectionManager;
+    private WalletMessageTaskManager messageTaskManager;
+    private boolean isServerRunning = true;
+
+    public PeerServer(Wallet wallet, PeerConnectionManager connectionManager, WalletMessageTaskManager messageTaskManager) throws IOException {
+        this.wallet = wallet;
+        this.connectionManager = connectionManager;
+        this.messageTaskManager = messageTaskManager;
+        server = new ServerSocket(Configuration.getPORT());
+        PeerServer.IPAddress = server.getInetAddress().getHostAddress();
+    }
+
+    public static String getIPAddress() {
+        return PeerServer.IPAddress;
+    }
+
+    @Override
+    public void run() {
+        LogManager.log(Configuration.getLogBarMax(), "Peer server of " + wallet.getName() + " is listening now");
+        while (isServerRunning) {
+            try {
+                if (connectionManager.numberOfExistingIncomingConnection() >= Configuration.getIncomingConnectionsLimit()) {
+                    Thread.sleep(Configuration.getThreadSleepTimeLong());
+                } else {
+                    Socket socket = server.accept();
+                    InetAddress clientAddress = socket.getInetAddress();
+                    LogManager.log(Configuration.getLogBarMax(), "Received an incoming connection request from " + clientAddress.getHostAddress());
+                    PeerIncomingConnection peer = new PeerIncomingConnection(wallet, socket, messageTaskManager, connectionManager);
+                    Thread thread = new Thread(peer);
+                    LogManager.log(Configuration.getLogBarMax(), "PeerIncomingConnection with " + peer.getConnectionIP() + " established");
+                    connectionManager.addIncomingConnection(peer);
+                }
+            } catch (Exception e) {
+                LogManager.log(Configuration.getLogBarMax(), "Exception in PeerServer.run() -> " + e.getMessage());
+                isServerRunning = false;
+            }
+        }
+        System.exit(0);
+    }
 }
