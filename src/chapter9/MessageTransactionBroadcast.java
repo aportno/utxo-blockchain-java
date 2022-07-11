2b357e7a984f4b1e155a2cbf3b7e631ff57d3d2f
aportno <alexander.portno@gmail.com>
2022-07-10 20:19:55 -0400

    update

--- src/chapter9/MessageTransactionBroadcast.java
+++ src/chapter9/MessageTransactionBroadcast.java
@@ -1,14 +1,17 @@
 package chapter9;
 
 import java.io.Serial;
+import java.security.PublicKey;
 
 public class MessageTransactionBroadcast extends Message {
     @Serial
     private static final long serialVersionUID = 1L;
     private final Transaction transaction;
+    private final long timeStamp;
 
     public MessageTransactionBroadcast(Transaction transaction) {
         this.transaction = transaction;
+        timeStamp = UtilityMethods.getTimeStamp();
     }
 
     public int getMessageType() {
@@ -19,7 +22,25 @@ public class MessageTransactionBroadcast extends Message {
         return this.transaction;
     }
 
+    public String getMessageHashID() {
+        return transaction.getHashID();
+    }
+
+    @Override
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public PublicKey getSenderKey() {
+        return transaction.getSender();
+    }
+
     public boolean isForBroadcast() {
         return true;
     }
+
+    protected boolean isSelfMessageAllowed() {
+        return true;
+    }
+
 }
