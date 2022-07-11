0dccdf85447291cf7addc42cb7e520f58333fdbe
aportno <alexander.portno@gmail.com>
2022-07-10 20:04:17 -0400

    update

---
+++ src/chapter9/MessageBroadcastMakingFriend.java
@@ -0,0 +1,71 @@
+package chapter9;
+
+import java.io.Serial;
+import java.security.Key;
+import java.security.PublicKey;
+import java.security.PrivateKey;
+
+public class MessageBroadcastMakingFriend extends Message {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private final String info;
+    private final byte[] signature;
+    private final PublicKey sender;
+    private final String senderName;
+    private final String uniqueHashID;
+    private final long timeStamp;
+    private final String ipAddress;
+
+    public MessageBroadcastMakingFriend(PrivateKey privateKey, PublicKey senderKey, String senderName, String ipAddress) {
+        this.sender = senderKey;
+        this.senderName = senderName;
+        this.ipAddress = ipAddress;
+        info = Message.JCOIN_MESSAGE;
+        signature = UtilityMethods.generateSignature(privateKey, info);
+        timeStamp = UtilityMethods.getTimeStamp();
+        String msg = UtilityMethods.getKeyString(senderKey) + senderName + UtilityMethods.getUniqueNumber() + timeStamp;
+        uniqueHashID = UtilityMethods.messageDigestSHA256_toString(msg);
+    }
+
+    @Override
+    public Object getMessageBody() {
+        return info;
+    }
+
+    @Override
+    public int getMessageType() {
+        return Message.ADDRESS_BROADCAST_MAKING_FRIEND;
+    }
+
+    @Override
+    public String getMessageHashID() {
+        return uniqueHashID;
+    }
+
+    @Override
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    @Override
+    public PublicKey getSenderKey() {
+        return sender;
+    }
+
+    @Override
+    public boolean isForBroadcast() {
+        return true;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public KeyNamePair getKeyNamePair() {
+        return new KeyNamePair(getSenderKey(), getSenderName());
+    }
+}
